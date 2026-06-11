67456581579070dadbbcac49599ad997ef4b599e
William Lee <43682783+wlee221@users.noreply.github.com>
2026-06-10 18:17:03 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox main</h1>
+      <h1>Hello CodeSandbox main 2</h1>
       <h2>test1</h2>
     </div>
   );
